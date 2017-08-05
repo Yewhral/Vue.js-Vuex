c55af09e42c4b65d7e1fb8127ec6a3166fbcb436
Yewhral <zabrzyjewski@gmail.com>
2017-08-05 07:25:53 +0200

    Project prepared with Vue, without Vuex

--- vuex-project/src/App.vue
+++ vuex-project/src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>
+  <div id="app">
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
 
   </div>
 </template>
 
 <script>
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+
 export default {
+    components:{
+        'product-list-one':ProductListOne,
+        'product-list-two':ProductListTwo
+    },
   name: 'app',
   data () {
     return {
-
+        products:[
+            {name:'Heat pump', price:80},
+            {name:'Boiler', price:60},
+            {name:'Water Heater', price:30},
+            {name:'Solar panel', price:50}
+        ]
+        }
     }
-  }
+
 }
 </script>
 
